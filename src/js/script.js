1f27de4253740acaf1138297ab9dc41eeb6ed1cb
Kaique Ferreira <kakamelo141205@gmail.com>
2023-06-26 12:44:05 -0300

    push correção

--- src/js/script.js
+++ src/js/script.js
@@ -3,23 +3,24 @@ const closeElement = document.querySelector(".close")
 
 const menuElement = document.querySelector("#menu-mobile")
 burgerElement.addEventListener('click', function(event) {
-   
-
-        menuElement.style.width = "65%";
+    menuElement.style.width = "65%";
     const visibleClose = closeElement.style.display = "none"
-
     if(visibleClose) {
+        burgerElement.style.display = "none"
         closeElement.style.display = "block"
         closeElement.addEventListener("click", () => {
             burgerElement.style.display = "block"
             closeElement.style.display = "none"
             menuElement.style.width = "0";
         })
-        burgerElement.style.display = "none"
     } else {
         closeElement.style.display = "block"
         burgerElement.style.display = "none"
     }
-
 })
 
+ClickLink = () => {
+    burgerElement.style.display = "block"
+    closeElement.style.display = "none"
+    menuElement.style.width = "0";
+}
